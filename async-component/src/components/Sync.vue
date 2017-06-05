c4e4d12c0644ff504c2b61aded6748ea38af697d
htank.wang <htank.wang@vipshop.com>
2017-06-05 16:21:34 +0800

    vue async component test

---
+++ async-component/src/components/Sync.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>this is sync page</h1>
+    <a href="#/">to Home</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    mounted () {
+      require.ensure(['../../static/js/jquery.min.js'], () => {
+        // do something
+      })
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
